bb4d50a7b9d12fd2518a13a174825fa46f92cd2a
onifade-joshua <onifadejoshua15@gmail.com>
2023-09-11 23:46:04 +0100

    Add project files.

---
+++ Example3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDone = true;
+            var count = 0;
+            while (isDone)
+            {
+
+                if(count < 10)
+                {
+                    Console.WriteLine("Hey you're counting this time!");
+                    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+                    ++count;
+                }else
+                {
+                    isDone = false;
+                }
+            }
+            Console.WriteLine("End of count try again!");
+            Console.WriteLine("Enter any keyword to continue...");
+            Console.ReadKey();
+        }
+    }
+}
